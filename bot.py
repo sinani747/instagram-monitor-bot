5e05f638bfe0240d7b0b7bbb80db49d6184e079b
beni sinani <96665420+sinani747@users.noreply.github.com>
2026-03-13 01:52:55 +0200

    Refactor bot.py to improve readability

--- bot.py
+++ bot.py
@@ -1,13 +1,17 @@
 import requests
-import os
 
-TOKEN = os.environ["TOKEN"]
-CHAT_ID = os.environ["CHAT_ID"]
+# ВСТАВЬ СЮДА СВОЙ ТОКЕН
+TOKEN = "8739941878:AAF3ZvpUlmenPixhJ1_hCJuOvnfWtcKINX0"
+
+# ВСТАВЬ СЮДА CHAT ID
+CHAT_ID = "473201462"
 
 def send(msg):
-    requests.post(
-        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-        data={"chat_id": CHAT_ID, "text": msg}
-    )
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": msg
+    }
+    requests.post(url, data=data)
 
-send("🚀 Instagram monitor работает")
+send("🚀 Instagram monitor работает!")
