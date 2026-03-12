27baff67eb9de1f696e58522c5f86f9aff2a113e
beni sinani <96665420+sinani747@users.noreply.github.com>
2026-03-13 01:11:13 +0200

    Refactor Instagram monitoring logic in bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,8 @@
 import requests
-import time
-from bs4 import BeautifulSoup
 import os
 
 TOKEN = os.environ["8739941878:AAF3ZvpUlmenPixhJ1_hCJuOvnfWtcKINX0"]
 CHAT_ID = os.environ["473201462"]
-
-URL = "https://www.instagram.com/teenageengineering/"
-
-headers = {
-"User-Agent": "Mozilla/5.0"
-}
-
-last_post = None
 
 def send(msg):
     requests.post(
@@ -20,34 +10,4 @@ def send(msg):
         data={"chat_id": CHAT_ID, "text": msg}
     )
 
-send("🚀 Instagram монитор запущен")
-
-while True:
-
-    try:
-
-        r = requests.get(URL, headers=headers)
-        soup = BeautifulSoup(r.text, "html.parser")
-
-        scripts = soup.find_all("script")
-        text = str(scripts)
-
-        start = text.find("/p/")
-        shortcode = text[start+3:start+14]
-
-        if last_post is None:
-            last_post = shortcode
-
-        if shortcode != last_post:
-
-            last_post = shortcode
-            link = f"https://instagram.com/p/{shortcode}"
-
-            send("🔥 Новый пост\n" + link)
-
-        time.sleep(60)
-
-    except Exception as e:
-
-        send("⚠️ Ошибка: " + str(e))
-        time.sleep(120)
+send("🚀 Instagram monitor работает")
